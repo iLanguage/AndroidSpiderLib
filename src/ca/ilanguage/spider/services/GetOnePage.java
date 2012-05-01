b81557a30152f8247ebbe5ec864732ebd0d4d7b9
trisapeace <trisapeace@gmail.com>
2012-05-01 17:09:22 -0400

    Changed intent extras to conform to conventions.

--- src/ca/ilanguage/spider/services/GetOnePage.java
+++ src/ca/ilanguage/spider/services/GetOnePage.java
@@ -17,15 +17,59 @@ import android.util.Log;
 
 public class GetOnePage extends IntentService {
 	public static final String TAG = "GetOnePage";
-	public static final String URL = "URL";
-	public static final String FILE_PREFIX = "prefix";
-	public static final String CONTENT_URI = "contentUri";
-	public static final String URL_COLUMN_NAME = "url";
-	public static final String HTML_FILE_COLUMN_NAME = "html";
-	public static final String TITLE_COLUMN_NAME = "title";
-	public static final String CREATED_COLUMN_NAME = "created";
-	public static final String MODIFIED_COLUMN_NAME = "modified";
-	public static final String MESSENGER = "messenger";
+	
+	/**
+	 * Key to the String that contains the Google Doc URL.
+	 */
+	public static final String EXTRA_URL = "ca.ilanguage.spider.services.intent.extra.URL";
+	
+	/**
+	 * Key to the String that contains the unique prefix that will be used to save the files
+	 * on the SD card.
+	 */
+	public static final String EXTRA_FILE_PREFIX = "ca.ilanguage.spider.services.intent.extra.prefix";
+	
+	/**
+	 * Key to the String containing the content URI of the database to which the URL's data will
+	 * be saved. 
+	 */
+	public static final String EXTRA_CONTENT_URI = "ca.ilanguage.spider.services.intent.extra.contentUri";
+	
+	/**
+	 * Key to the String containing the name of the column in the database where the URL should
+	 * be stored.
+	 */
+	public static final String EXTRA_URL_COLUMN_NAME = "ca.ilanguage.spider.services.intent.extra.url";
+	
+	/**
+	 * Key to the String containing the name of the column in the database where the SD card file 
+	 * location of the HTML should be stored.
+	 */
+	public static final String EXTRA_HTML_FILE_COLUMN_NAME = "ca.ilanguage.spider.services.intent.extra.html";
+	
+	/**
+	 * Key to the String containing the name of the column in the database where the title of the
+	 * HTML page should be stored.
+	 */
+	public static final String EXTRA_TITLE_COLUMN_NAME = "ca.ilanguage.spider.services.intent.extra.title";
+	
+	/**
+	 * Key to the String containing the name of the column in the database where the created date
+	 * of the row should be stored.
+	 */
+	public static final String EXTRA_CREATED_COLUMN_NAME = "ca.ilanguage.spider.services.intent.extra.created";
+	
+	/**
+	 * Key to the String containing the name of the column in the database where the modified date
+	 * of the row should be stored.
+	 */
+	public static final String EXTRA_MODIFIED_COLUMN_NAME = "ca.ilanguage.spider.services.intent.extra.modified";
+	
+	/**
+	 * Key to the Messenger to which the SpiderResult is to be sent (or null, if the caller does
+	 * not care about the result).
+	 */
+	public static final String EXTRA_MESSENGER = "ca.ilanguage.spider.services.intent.extra.messenger";
 
 	public GetOnePage() {
 		super("GetOnePage");
@@ -37,7 +81,7 @@ public class GetOnePage extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		Log.d(TAG, "I am going to spider this URL: " + intent.getStringExtra(GetOnePage.URL));
+		Log.d(TAG, "I am going to spider this URL: " + intent.getStringExtra(GetOnePage.EXTRA_URL));
 
 		Spider spider = null;
 		String fileLocation = "";
@@ -45,29 +89,29 @@ public class GetOnePage extends IntentService {
 		
 		try {
 			// Create a Spider to spider the HTML of the given URL
-			spider = new Spider(intent.getStringExtra(GetOnePage.URL));
+			spider = new Spider(intent.getStringExtra(GetOnePage.EXTRA_URL));
 	
 			if (SdCardDao.isWritable()) {
 				// Save the linked CSS file(s) and modify the HTML to point to the new location(s)
-				HashMap<String, String> cssLinks = spider.getAndReplaceCss(intent.getStringExtra(FILE_PREFIX) + "link", ".css");
+				HashMap<String, String> cssLinks = spider.getAndReplaceCss(intent.getStringExtra(EXTRA_FILE_PREFIX) + "link", ".css");
 				for (String newLink : cssLinks.keySet()) {
 					SdCardDao.downloadFromUrl(cssLinks.get(newLink), getApplicationContext().getExternalFilesDir(null).toString(), newLink);
 				}
 				
 				// Save the CSS file(s) imported in the <style> tag and modify the HTML to point to the new location(s)
-				HashMap<String, String> importLinks = spider.getAndReplaceImports(intent.getStringExtra(FILE_PREFIX) + "import", ".css");
+				HashMap<String, String> importLinks = spider.getAndReplaceImports(intent.getStringExtra(EXTRA_FILE_PREFIX) + "import", ".css");
 				for (String newLink : importLinks.keySet()) {
 					SdCardDao.downloadFromUrl(importLinks.get(newLink), getApplicationContext().getExternalFilesDir(null).toString(), newLink);
 				}
 				
 				// Save the files referenced by the CSS in the <style> tag and modify the HTML to point to the new location(s)
-				HashMap<String, String> fileLinks = spider.getAndReplaceUrls(intent.getStringExtra(FILE_PREFIX) + "url", "");
+				HashMap<String, String> fileLinks = spider.getAndReplaceUrls(intent.getStringExtra(EXTRA_FILE_PREFIX) + "url", "");
 				for (String newLink : fileLinks.keySet()) {
 					SdCardDao.downloadFromUrl(fileLinks.get(newLink), getApplicationContext().getExternalFilesDir(null).toString(), newLink);
 				}
 				
 				// Save the modified HTML
-				fileLocation = SdCardDao.writeToFile(spider.getHtml(), getApplicationContext().getExternalFilesDir(null).toString(), intent.getStringExtra(FILE_PREFIX) + "index.html");
+				fileLocation = SdCardDao.writeToFile(spider.getHtml(), getApplicationContext().getExternalFilesDir(null).toString(), intent.getStringExtra(EXTRA_FILE_PREFIX) + "index.html");
 			} else {
 				Log.d(TAG, "Could not write to SD card.");
 			}
@@ -77,28 +121,28 @@ public class GetOnePage extends IntentService {
 	
 			// Insert a row into the database with the URL and the file location of
 			// its HTML.
-			rowId = insertUrlIntoDatabase(intent.getStringExtra(GetOnePage.CONTENT_URI),
-					intent.getStringExtra(GetOnePage.URL_COLUMN_NAME),
-					intent.getStringExtra(GetOnePage.URL),
-					intent.getStringExtra(GetOnePage.HTML_FILE_COLUMN_NAME),
+			rowId = insertUrlIntoDatabase(intent.getStringExtra(GetOnePage.EXTRA_CONTENT_URI),
+					intent.getStringExtra(GetOnePage.EXTRA_URL_COLUMN_NAME),
+					intent.getStringExtra(GetOnePage.EXTRA_URL),
+					intent.getStringExtra(GetOnePage.EXTRA_HTML_FILE_COLUMN_NAME),
 					fileLocation,
-					intent.getStringExtra(GetOnePage.TITLE_COLUMN_NAME),
+					intent.getStringExtra(GetOnePage.EXTRA_TITLE_COLUMN_NAME),
 					title,
-					intent.getStringExtra(GetOnePage.CREATED_COLUMN_NAME),
-					intent.getStringExtra(GetOnePage.MODIFIED_COLUMN_NAME));
+					intent.getStringExtra(GetOnePage.EXTRA_CREATED_COLUMN_NAME),
+					intent.getStringExtra(GetOnePage.EXTRA_MODIFIED_COLUMN_NAME));
 		} catch (IOException e) {
-			Log.e(TAG, "Error parsing URL: " + intent.getStringExtra(URL));
+			Log.e(TAG, "Error parsing URL: " + intent.getStringExtra(EXTRA_URL));
 		}
 		
 		// Send a Message back to the caller, if they wanted one
 		// Code based on: http://www.vogella.com/articles/AndroidServices/article.html#tutorial_intentservice
 		Bundle extras = intent.getExtras();
 		if (extras != null) {
-			Messenger messenger = (Messenger) extras.get(MESSENGER);
+			Messenger messenger = (Messenger) extras.get(EXTRA_MESSENGER);
 			// If the user gave us a Messenger
 			if (messenger != null) {
 				Message msg = Message.obtain();
-				msg.obj = new SpiderResult((spider != null) && spider.isSpiderInitialized(), intent.getStringExtra(URL), fileLocation, rowId);
+				msg.obj = new SpiderResult((spider != null) && spider.isSpiderInitialized(), intent.getStringExtra(EXTRA_URL), fileLocation, rowId);
 				try {
 					messenger.send(msg);
 				} catch (android.os.RemoteException e) {
